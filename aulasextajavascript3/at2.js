6a4c59b309a53e4aabb2dc345a35feca7bd28a88
Uthiele Guimaraes <uthieli21@gmail.com>
2023-11-10 21:20:39 -0300

    Update at2.js

--- aulasextajavascript3/at2.js
+++ aulasextajavascript3/at2.js
@@ -68,28 +68,33 @@ reatribua valores às variáveis declaradas por ela, o que será útil quando o 
 que uma variável precisa armazenar muda ao longo do tempo.*/
 
 
-let  changeMe1 = false;
-console.log(changeMe1);
+let  ChangeMe = false;
+console.log(ChangeMe);
 
 /*8)Para verificar se "changeMe" foi reatribuído com sucesso, imprima o valor salvo
 em "changeMe" no console usando "console.log()". Essa é uma boa prática para
 garantir que suas reatribuições de variáveis estão funcionando conforme
 esperado.*/
 
+let changeME = 'Primeiro valor';
 
-console.log();
+changeME = 'Novo valor';
+
+console.log(changeME);
 
 /*9)Agora, vamos explorar a declaração de variáveis com "const". Declare uma
 variável constante chamada "entree" e atribua a ela a string 'Enchiladas'. "const"
 é uma palavra-chave que declara uma variável cujo valor não pode ser reatribuído
 depois.*/
-console.log();
+
+const entree = "Enchiladas"
 
 /*10)Para verificar se você salvou o valor de 'Enchiladas' em "entree" corretamente,
 imprima o valor de "entree" no console usando "console.log()".*/
 
+const entreE = 'Enchiladas';
 
-console.log();
+console.log(entreE);
 
 /*11)Vamos testar a imutabilidade de variáveis declaradas com "const". Tente
 reatribuir um valor diferente à variável "entree". Isso deve gerar um erro, pois
@@ -97,7 +102,9 @@ uma variável "const" não pode ser reatribuída após a sua declaração inicia
 é um importante conceito em JavaScript que ajuda a prevenir erros indesejados
 de reatribuição.*/
 
-console.log();
+const entrEE = 'Enchiladas';
+
+entrEE = 'Sushi';
 
 /*12)No JavaScript, temos operadores de atribuição que nos permitem atualizar
 rapidamente o valor de uma variável com base em seu valor atual. Por exemplo,
@@ -105,16 +112,23 @@ o operador "+=" adiciona o valor à direita ao valor atual da variável. Utilize
 operador "+=" para aumentar o valor armazenado em uma variável "levelUp" por
 5.*/
 
-console.log();
+let levelUp = 10;
+
+
+levelUp += 5;
+
+
+console.log(levelUp);
 
 /*13)Da mesma forma, o operador "-=" subtrai o valor à direita do valor atual da
 variável. Use o operador "-=" para diminuir o valor armazenado em uma variável
 "powerLevel" por 100.*/
 
-console.log();
+let powerLevel = 500;
 
+powerLevel -= 100;
 
-
+console.log(powerLevel);
 
 /*
 
@@ -123,27 +137,42 @@ console.log();
 operador. Use o operador “*=“ para multiplicar o valor armazenado em uma
 variável "multiplyMe" por 11.*/
 
-console.log();
+let multiplyMe = 7;
+
+multiplyMe *= 11;
+
+console.log(multiplyMe);
 
 /*15)O operador "/=" divide o valor atual da variável pelo valor à direita do
 operador. Use o operador "/=" para dividir o valor armazenado em uma variável
 "quarterMe" por 4.*/
 
-console.log();
+let quarterMe = 16;
+
+quarterMe /= 4;
+
+console.log(quarterMe);
 
 /*16)O operador "++" é conhecido como operador de incremento. Ele aumenta o
 valor da variável em 1. Usando o operador de incremento, aumente o valor de
 uma variável "gainedDollar" que inicialmente é declarada valendo 1.*/
 
+let gainedDollar = 1;
 
-console.log();
+gainedDollar++;
+
+console.log(gainedDollar);
 
 
 /*17)De maneira semelhante, o operador "--" é o operador de decremento. Ele
 diminui o valor da variável em 1. Usando o operador de decremento, diminua o
 valor de uma variável "lostDollar".*/
 
-console.log();
+let lostDollar = 5;
+
+lostDollar--;
+
+console.log(lostDollar);
 
 
 /*18)No JavaScript, podemos usar "template literals" para incorporar expressões
@@ -153,13 +182,19 @@ suas variáveis na frase "My name is NAME. My favorite city is CITY." Substitua
 "NAME" e "CITY" na string acima interpolando os valores salvos em variáveis
 "myName" e "myCity". Use "console.log()" para imprimir sua frase no console.*/
 
+let myName = 'John';
+let myCity = 'New York';
 
-console.log(); 
+let myMessage = `My name is ${myName}. My favorite city is ${myCity}.`;
+
+console.log(myMessage);
 
 /*19)O operador "typeof" em JavaScript retorna uma string indicando o tipo de
 dado de uma variável. Use "console.log()" para imprimir o tipo de uma variável
 
 "newVariable". Isso pode ser útil para debugar seu código se você não tem
 certeza do tipo de uma variável em algum momento.*/
-console.log();
 
+let newVariable = 42;
+
+console.log(typeof newVariable);
